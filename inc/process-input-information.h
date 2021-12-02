99f990b35beb2d3d37522a6bd32045973980d9f7
mero <meroa8634@gmail.com>
2021-12-02 14:42:00 +0200

    add process info structure to store input in

---
+++ inc/process-input-information.h
@@ -0,0 +1,11 @@
+#ifndef PROCESS_INPUT_INFORMATION_H
+#define PROCESS_INPUT_INFORMATION_H
+
+struct process_input_information
+{
+    int id, arriv_time, tot_run_time, priority; 
+};
+
+typedef struct process_input_information piinfo;
+
+#endif
