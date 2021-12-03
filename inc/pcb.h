7a4757deb9ddf4219a7ddeebf540ac2e457bb836
mahmed32 <meroa8634@gmail.com>
2021-12-03 02:05:07 +0200

    set up pcb structure

---
+++ inc/pcb.h
@@ -0,0 +1,27 @@
+#ifndef PCB_H
+#define PCB_H
+
+#include "process-input-information.h"
+
+typedef enum process_state
+{
+    NONE_PS = 0,
+    STARTED,
+    RESUMED,
+    STOPPED,
+    FINISHED
+} process_state;
+
+typedef struct process_control_block
+{
+    process_state curr_state;
+    piinfo given_info; /*given from the generator*/
+    int last_time_leave_cpu,
+        remain_time,
+        wait_time,
+        turn_around_time;
+    float weighted_turn_around;
+} pcb;
+
+#endif
+
