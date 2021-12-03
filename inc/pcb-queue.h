b8e0588b5b35d01e14f628571ae8b3f3587082a3
mahmed32 <meroa8634@gmail.com>
2021-12-03 03:51:50 +0200

    add interface for pcb queue

---
+++ inc/pcb-queue.h
@@ -0,0 +1,22 @@
+#ifndef PCB_QUEUE_H
+#define PCB_QUEUE_H
+
+#include "pcb.h"
+
+typedef struct pcb_queue
+{
+    int size, read_index, write_index;
+    PCB* pcbs;
+} pcb_queue;
+
+pcb_queue* create_pcb_queue(const int);
+
+int is_full_pcb_queue(pcb_queue*);
+
+int is_empty_pcb_queue(pcb_queue*);
+
+void enqueue_pcb(pcb_queue*, PCB);
+
+PCB dequeue_pcb(pcb_queue*);
+
+#endif
