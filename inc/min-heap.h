ceea84abeb9e7ae4cbb237e69c78de854343c7c0
mahmed32 <meroa8634@gmail.com>
2021-12-03 13:22:03 +0200

    set up interface for min heap

---
+++ inc/min-heap.h
@@ -0,0 +1,25 @@
+#ifndef MIN_HEAP_H
+#define MIN_HEAP_H
+
+#include "pcb.h"
+
+typedef struct pcb_min_heap
+{
+    int lenght;
+    PCB* pcbs;
+
+} pcb_heap;
+
+pcb_heap create_pcb_heap(const int);
+
+void free_pcb_heap(pcb_heap*);
+
+int is_empty_pcb_heap(pcb_heap*);
+
+PCB get_min_pcb_heap(pcb_heap*);
+
+PCB extract_min_pcb_heap(pcb_heap*);
+
+void insert_pcb_heap(pcb_heap*, PCB);
+
+#endif
