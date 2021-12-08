bfdc952f3407dc31b34f4063838b844c4ae4ae25
mahmed32 <meroa8634@gmail.com>
2021-12-08 16:38:40 +0200

    remove shared memory resources

--- inc/shm-resources.h
+++ inc/shm-resources.h
@@ -5,19 +5,6 @@
 #include <sys/shm.h>
 
 #define FTOK_FILE_NAME "./inc/ftok.key"
-#define PROCESSES_LIST_PROJECT_ID          0 
-#define NUM_ARRIVED_PROCESSES_PROJECT_ID   1
-#define NUM_READY_PROCESSES_PROJECT_ID     2 
-#define CLK_PROJECT_ID                     3 
-
-#define GENERATOR_FLAG  IPC_CREAT | IPC_EXCL | 0666
-#define NOT_GENERATOR_FLAG 0666
-
-typedef struct shm_res_ids
-{
-    int processes_list_id, num_arrived_processes_id,
-        num_ready_processes_id, clk_id;
-} shm_res_ids;
 
 #endif
 
